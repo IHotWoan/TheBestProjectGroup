be1d5be16f66d4d391db982797a1a8766af51563
fredrik9 <fj222hn@student.lnu.se>
2016-04-25 00:16:23 +0200

    Added files via upload

---
+++ WebContent/script.js
@@ -0,0 +1,20 @@
+document.getElementById("nav01").innerHTML =
+"<header id='header'>"+
+"<a href='Test.html'><img src='best.jpg' alt='Logo placeholder' style='width: 300px; height: 200px;' /></a>"+
+"<h1> Administrator </h1>"+
+"<h1> Username </h1>"+
+"<p id='demo'></p>"+
+"<h2><a href='otherpage.xhtml'> Log out</a></h2>"+
+"</header>";
+
+
+document.getElementById('demo').innerHTML = Date();
+
+document.getElementById("nav02").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='dashboard.xhtml'>Dashboard</a></li>"+
+"<li><a href='manageorders.xhtml'>Manage orders</a></li>"+
+"<li><a href='manageproducts.xhtml'>Manage products</a></li>"+
+"<li><a href='manageusers.xhtml'>Manage users</a></li>"+
+"</ul>";
+
