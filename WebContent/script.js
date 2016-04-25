1b8668fc7b5a8e042ad79a291b50281a7bf85ac8
fredrik9 <fj222hn@student.lnu.se>
2016-04-25 21:06:40 +0200

    basic layout finished

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,14 +1,19 @@
+
+//header
+
 document.getElementById("nav01").innerHTML =
 "<header id='header'>"+
 "<a href='dashboard.xhtml'><img src='best.jpg' alt='Logo placeholder' style='width: 300px; height: 200px;' /></a>"+
 "<h1> Administrator </h1>"+
-"<h1> Username </h1>"+
-"<p id='demo'></p>"+
+"<h1> \" Username \" </h1>"+
+"<p id='date'></p>"+
 "<h2><a href='otherpage.xhtml'> Log out</a></h2>"+
 "</header>";
 
+document.getElementById('date').innerHTML = Date();
 
-document.getElementById('demo').innerHTML = Date();
+
+//navigation menu
 
 document.getElementById("nav02").innerHTML =
 "<ul id='menu'>" +
@@ -17,4 +22,6 @@ document.getElementById("nav02").innerHTML =
 "<li><a href='manageproducts.xhtml'>Manage products</a></li>"+
 "<li><a href='manageusers.xhtml'>Manage users</a></li>"+
 "</ul>";
+
+
 
