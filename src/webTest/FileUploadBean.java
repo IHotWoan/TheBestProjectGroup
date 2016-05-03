1abcebc5c519f76372fc4b6c2a48442369565836
Songho Lee <sl222xk@student.lnu.se>
2016-05-04 01:52:41 +0200

    MANAGED TO UPLOAD IMAGE TO DATABASE AND GET IMAGE FROM DATABASE!!

---
+++ src/webTest/FileUploadBean.java
@@ -0,0 +1,114 @@
+/**
+ * 
+ */
+package webTest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.persistence.Lob;
+import javax.servlet.http.Part;
+
+/**
+ * @author songhokun
+ *
+ */
+@SessionScoped
+@ManagedBean(name="fileuploadbean")
+public class FileUploadBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3474817816894473632L;
+	private Part file;
+	private int productID=1;
+	
+	@Lob
+    private byte[] image;
+	  
+	public String upload(){
+		try{
+		  InputStream inputStream = file.getInputStream();   
+		  
+		  ByteArrayOutputStream output = new ByteArrayOutputStream();
+		  byte[] buffer = new byte[10240000];
+		  for (int length = 0; (length = inputStream.read(buffer)) > 0;) output.write(buffer, 0, length);
+		  
+		  this.setImage(output.toByteArray());
+		  
+		  try {
+			  MysqlConnect db = new MysqlConnect();
+			  Connection conn = db.conn;
+			  PreparedStatement statement = conn.prepareStatement("UPDATE products SET product_image =? WHERE product_id=?");
+			  
+			  statement.setBytes(1, this.image);
+			  //statement.setBinaryStream(1, inputStream);
+			  statement.setInt(2, productID);
+			  statement.executeUpdate();
+			  
+			  db.close();
+			  db=null;
+			  
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  
+	        return "success";
+		}
+		catch(IOException e){
+			System.err.println("ERROR OCCURRED IN FILE UPLOAING");
+			e.printStackTrace();
+		}
+	        return "failure";
+	}
+	public Part getFile() {
+		  return file;
+	  }
+	 
+	  public void setFile(Part file) {
+		  this.file = file;
+	  }
+	  public void validateFile(FacesContext ctx, UIComponent comp, Object value) {
+		  List<FacesMessage> msgs = new ArrayList<FacesMessage>();
+		  Part file = (Part)value;
+		  /*
+		  if (file.getSize() > 102400) {
+			  msgs.add(new FacesMessage("file too big"));
+		  	}
+		  
+		  if (!"text/plain".equals(file.getContentType())) {
+			  msgs.add(new FacesMessage("not a text file"));
+			  }
+			 */
+		  if (!msgs.isEmpty()) {
+			  throw new ValidatorException(msgs);
+		  }
+	  }
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public byte[] getImage() {
+		return image;
+	}
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	  
+}
---
+++ src/webTest/ProductImageServlet.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package webTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * @author songhokun
+ *
+ */
+@WebServlet("/productImageServlet")
+public class ProductImageServlet extends HttpServlet{
+	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        Long productID = Long.valueOf(request.getParameter("id"));
+
+	        MysqlConnect db = new MysqlConnect();
+	        try {
+	        	String query="SELECT product_image, LENGTH(product_image) AS imageContentLength FROM products WHERE product_ID="+productID;
+	            ResultSet resultSet = db.query(query);
+
+	            if (resultSet.next()) {
+	            	response.setContentType("image/jpeg");
+	            	//response.setContentType(getServletContext().getMimeType(String.valueOf(productID)));
+	                response.setContentLength(resultSet.getInt("imageContentLength"));
+	                response.setHeader("Content-Disposition", "inline;filename=\"" + productID + "\"");
+
+	                ReadableByteChannel input = null;
+	                WritableByteChannel output = null;
+	                
+	                Blob imageBlob = resultSet.getBlob("product_image");
+	                
+	                InputStream in = imageBlob.getBinaryStream();
+	                OutputStream out = response.getOutputStream();
+	                int b;
+	                while ((b = in.read()) != -1) {
+	                    out.write(b);
+	                }
+
+	                in.close();
+	                out.flush();
+	                out.close();
+	            } else {
+	                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+	            }
+	            db.close();
+	            db=null;
+	            
+	        } catch (SQLException e) {
+	            throw new ServletException("Something failed at SQL/DB level.", e);
+	    }
+	 }
+}
